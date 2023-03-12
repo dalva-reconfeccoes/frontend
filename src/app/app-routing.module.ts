d08627055019cc8cf11a5c49804eeb8f7abe74fb
Eduardo Morais <eduardo.rodrigues@under.com.br>
2023-03-12 20:38:17 -0300

    Feature - Módulo de carrinho e inclusão de dados em local storage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProductsComponent} from './products/products.component'
-import { HomeComponent} from './home/home.component'
-import { ContactComponent} from './contact/contact.component'
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 import { SelectedProductComponent } from './selected-product/selected-product.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },    
-  { path: 'products', component: ProductsComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: `product/:uuid`, component: SelectedProductComponent },
-  
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'cart', component: CartComponent },
+    { path: `product/:uuid`, component: SelectedProductComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/selected-product/selected-product.component.ts
+++ src/app/selected-product/selected-product.component.ts
@@ -1,123 +1,156 @@
 import { Component } from '@angular/core';
-import { PrimeNGConfig } from 'primeng/api';
-import {MessageService} from 'primeng/api';
-import { Router } from '@angular/router'
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
+import { List } from 'postcss/lib/list';
 
 @Component({
-  selector: 'app-selected-product',
-  templateUrl: './selected-product.component.html',
-  styleUrls: ['./selected-product.component.scss']
+    selector: 'app-selected-product',
+    templateUrl: './selected-product.component.html',
+    styleUrls: ['./selected-product.component.scss'],
 })
 export class SelectedProductComponent {
-  product: any
-  image: any
-  cep: string
-  loadingCep: boolean = false
-  isValidCep: boolean = false
-  selectedFreight: any
-  quantitySelected: number = 1
-  responsiveOptions: any[] = [
-    {
-      breakpoint: '1024px',
-      numVisible: 5
-    },
-    {
-      breakpoint: '768px',
-      numVisible: 3
-    },
-    {
-      breakpoint: '560px',
-      numVisible: 3
+    product: any;
+    image: any;
+    cep: string;
+    loadingCep: boolean = false;
+    isValidCep: boolean = false;
+    selectedFreight: any;
+    quantitySelected: number = 1;
+    responsiveOptions: any[] = [
+        {
+            breakpoint: '1024px',
+            numVisible: 5,
+        },
+        {
+            breakpoint: '768px',
+            numVisible: 3,
+        },
+        {
+            breakpoint: '560px',
+            numVisible: 3,
+        },
+    ];
+    selectedSize: any;
+    sizes: any[] = [
+        { name: 'PP', code: 1 },
+        { name: 'P', code: 2 },
+        { name: 'M', code: 3 },
+        { name: 'G', code: 4 },
+        { name: 'GG', code: 5 },
+        { name: 'XGG', code: 6 },
+    ];
+
+    freightOptions: any[] = [
+        { name: 'SEDEX', value: 32.11, deliveryTime: '3', arrivalDay: '17/03' },
+        { name: 'PAC', value: 21.34, deliveryTime: '6', arrivalDay: '20/03' },
+    ];
+
+    displayDialog: boolean = false;
+    budget: any;
+
+    constructor(
+        private messageService: MessageService,
+        private router: Router
+    ) {}
+
+    ngOnInit() {
+        this.product = {
+            uuid: '5ccc2df9-107f-4b63-82a4-5c5ae17cf658',
+            name: 'tst6',
+            description: 'Camiseta Comum Preta',
+            color: 'Preto',
+            price: 121,
+            images: [
+                'preto/camiseta-preta-frente-masc.png',
+                'preto/camiseta-preta-costas-masc.png',
+                'preto/camiseta-preta-modelo-masc.png',
+                'preto/camiseta-preta-frente-masc.png',
+                'preto/camiseta-preta-costas-masc.png',
+                'preto/camiseta-preta-modelo-masc.png',
+            ],
+            knitted: '100% Algodão',
+            type: 'Camiseta',
+            subType: 'Comum',
+            quantity: 72,
+            sex: 'M',
+            inventoryStatus: 'DISPONIVEL',
+        };
     }
-  ];
-  selectedSize: any
-  sizes: any[] = [
-    {name: 'PP', code: 1},
-    {name: 'P', code: 2},
-    {name: 'M', code: 3},
-    {name: 'G', code: 4},
-    {name: 'GG', code: 5},
-    {name: 'XGG', code: 6}
-  ];
 
-  freightOptions: any[] = [
-    {name: 'SEDEX', value: 32.11, deliveryTime: "3", arrivalDay: "17/03" },
-    {name: 'PAC', value: 21.34, deliveryTime: "6", arrivalDay: "20/03"}
-  ];
-
-  displayDialog: boolean=false
-
-
-  constructor(private messageService: MessageService, private router: Router) {}
-
-  ngOnInit() {
-    this.product = {
-      uuid: "5ccc2df9-107f-4b63-82a4-5c5ae17cf658",
-      name: "tst6",
-      description: "Camiseta Comum Preta",
-      color:"Preto",
-      price: 121,
-      images: [
-        "preto/camiseta-preta-frente-masc.png",
-        "preto/camiseta-preta-costas-masc.png", 
-        "preto/camiseta-preta-modelo-masc.png",
-        "preto/camiseta-preta-frente-masc.png",
-        "preto/camiseta-preta-costas-masc.png", 
-        "preto/camiseta-preta-modelo-masc.png", 
-        
-      ],
-      knitted: "100% Algodão",
-      type: "Camiseta",
-      subType: "Comum",
-      quantity: 72,
-      sex: "M",
-      inventoryStatus: "DISPONIVEL"
+    onClick(event) {
+        console.log(event);
     }
-    
-  }
 
-  onClick(event){
-    console.log(event)
-  }
-
-  calculateFreight(){
-    this.loadingCep = true
-    
-    var re = /^([\d]{2})\.*([\d]{3})-*([\d]{3})/; // Pode usar ? no lugar do *
-    console.log(re.test(this.cep))
-    if(re.test(this.cep)){
-      console.log("CEP VALIDO", this.cep)
-      var clientCep = this.cep.replace(re,"$1$2$3")
-      this.isValidCep = true
-      console.log(clientCep)
-    }else{
-      console.log("CEP inválido!", this.cep);
-      this.messageService.add({severity:'error', summary:'CEP', detail:'Cep inválido!'})
+    calculateFreight() {
+        this.loadingCep = true;
+        var re = /^([\d]{2})\.*([\d]{3})-*([\d]{3})/;
+        if (re.test(this.cep)) {
+            console.log('CEP VALIDO', this.cep);
+            var clientCep = this.cep.replace(re, '$1$2$3');
+            this.isValidCep = true;
+            console.log(clientCep);
+        } else {
+            console.log('CEP inválido!', this.cep);
+            this.messageService.add({
+                severity: 'error',
+                summary: 'CEP',
+                detail: 'Cep inválido!',
+            });
+        }
+        setTimeout(() => (this.loadingCep = false), 1000);
     }
-    setTimeout(() => this.loadingCep = false, 1000);
-  }
-  
-  buyProduct(){
-    if(this.validateSelection()){
-      this.displayDialog=true
+
+    buyProduct() {
+        if (this.validateSelection()) {
+            this.budget = {
+                product: this.product,
+                selectedSize: this.selectedSize,
+                cep: this.cep,
+                selectedFreight: this.selectedFreight,
+                quantitySelected: this.quantitySelected,
+            };
+            this.displayDialog = true;
+        }
     }
-    console.log(this.selectedSize)
-    console.log(this.cep)
-    console.log(this.selectedFreight)
-    console.log(this.quantitySelected)
-  }
 
-  validateSelection(){
-    var isValid = true
-    if(!this.selectedSize){
-      this.showMessage("error", "Tamanho não informado","Selecione um tamanho pra continuar.")
-      isValid = false 
+    validateSelection() {
+        var isValid = true;
+        if (!this.selectedSize) {
+            this.showMessage(
+                'error',
+                'Tamanho não informado',
+                'Selecione um tamanho pra continuar.'
+            );
+            isValid = false;
+        }
+        return isValid;
     }
-    return isValid
-  }
 
-  showMessage(type: string, summary: string, detail: string){
-    this.messageService.add({severity:type, summary:summary, detail:detail})
-  }
+    showMessage(type: string, summary: string, detail: string) {
+        this.messageService.add({
+            severity: type,
+            summary: summary,
+            detail: detail,
+        });
+    }
 
+    navigateToProducts() {
+        this.router.navigate([`/products/`]);
+    }
+
+    setProductLocalStorage() {
+        var cart: Array<any> = JSON.parse(localStorage.getItem('cart'));
+        console.log(cart);
+        if (!cart) {
+            localStorage.setItem('cart', JSON.stringify([this.budget]));
+            return;
+        }
+        cart.push(this.budget);
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }
+
+    navigateToCart() {
+        this.setProductLocalStorage();
+        this.router.navigate([`/cart/`]);
+    }
 }
